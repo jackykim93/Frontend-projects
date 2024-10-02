772acccc2ecfa90616b9a56ed73b431305ac8ee6
jackyim93 <jackykim93@gmail.com>
2024-10-02 17:07:21 +0900

    App.jsx 파일 추가

---
+++ react-sass-youtube-clone/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import SearchedVideoPage from './pages/SearchedVideoPage';
+import VideoPage from './pages/VideoPage';
+import NavigationBar from './components/NavigationBar';
+import SideBar from './components/SideBar';
+
+const Layout = () => {
+  return (
+    <>
+      <NavigationBar />
+      <SideBar />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path='/results/:input' element={<SearchedVideoPage />} />
+          <Route path='/video/:videoId' element={<VideoPage />} />
+        </Route>
+      </Routes>
+  );
+}
+
+export default App;
