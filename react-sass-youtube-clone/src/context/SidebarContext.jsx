c565eeeeeeaede2978124b8e69d11c236bac4758
jackyim93 <jackykim93@gmail.com>
2024-10-02 17:01:52 +0900

    Sidebar, Search context 파일 추가

---
+++ react-sass-youtube-clone/src/context/SidebarContext.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { createContext } from "react";
+import React from 'react';
+
+export const SidebarContext = createContext();
+
+export const SidebarContextProvider = (props) => {
+    const [isToggled, setIsToggled] = useState(true);
+
+    const handleToggleSidebar = () => {
+        setIsToggled(!isToggled);
+    }
+
+    return (
+        <SidebarContext.Provider
+            value={{ isToggled, setIsToggled, handleToggleSidebar }}
+            {...props}
+        />
+    )
+}
