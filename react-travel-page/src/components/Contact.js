7263ec86793a1b571b92e66caf61e22cc09cb732
jackyim93 <jackykim93@gmail.com>
2024-10-15 02:12:29 +0900

    Contact us page for react-travel-page added

---
+++ react-travel-page/src/components/Contact.js
@@ -0,0 +1,35 @@
+import React from "react";
+import '../components/css/Contact.css';
+
+const Contact = () => {
+    return (
+        <section className="contact">
+            <div className="contact-container">
+                <div className="text-center">
+                    <h2 className="heading-contact">Contact Us</h2>
+                    <p className="contact-sub">Feel free to contact us with any problems or feedbacks</p>
+                </div>
+                <div className="form-container">
+                    <form className="form">
+                        <input className="name"
+                            type="text"
+                            placeholder="Enter your Name" 
+                        />
+                        <input className="email"
+                            type="text"
+                            placeholder="Enter your Email"
+                        />
+                        <textarea className="message"
+                            type="message"
+                            placeholder="Enter your Message"
+                            rows="10"
+                        />
+                        <button className="contact-button">Send us a Message</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Contact
