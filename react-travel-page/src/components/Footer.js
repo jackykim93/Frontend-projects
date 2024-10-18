e14383d952c82924271ae8946e5bcef2ad345987
jackyim93 <jackykim93@gmail.com>
2024-10-18 16:46:49 +0900

    updated Footer.js css import directory

--- react-travel-page/src/components/Footer.js
+++ react-travel-page/src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
-import '../components/css/Footer.css';
+import '../css/Footer.css';
 
 const Footer = () => {
     return (
