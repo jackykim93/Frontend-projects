223c5ae097f26434a00886f0d0ca3272fda61943
jackyim93 <jackykim93@gmail.com>
2024-10-21 19:39:20 +0900

    Home page added

---
+++ react-travel-page/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import WorldMap from './Worldmap';
+import Slide from './Swiper';
+import Contact from './Contact';
+import Footer from './Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <WorldMap />
+      <Slide />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
