8cd1e71a01a74384fe4427a49464df979bb98cc7
jackyim93 <jackykim93@gmail.com>
2024-10-23 16:01:19 +0900

    Navitems for navbar list added

---
+++ react-travel-page/src/components/Navitems.js
@@ -0,0 +1,44 @@
+export const navItems = [
+    {
+        id: 1,
+        title: "Home",
+        path: "/",
+        cName: "nav-item",
+    },
+    {
+        id: 2,
+        title: "About",
+        path: "/about",
+        cName: "nav-item",
+    },
+    {
+        id: 3,
+        title: "Worldmap",
+        path: "highcharts-container",
+        cName: "nav-item",
+    },
+    {
+        id: 4,
+        title: "Recommendations",
+        path: "swiper-section",
+        cName: "nav-item",
+    },
+    {
+        id: 5,
+        title: "Contact",
+        path: "contact",
+        cName: "nav-item",
+    },
+    {
+        id: 6,
+        title: "Hotels",
+        path: "#",
+        cName: "nav-item",
+    },
+    {
+        id: 7,
+        title: "Airlines",
+        path: "#",
+        cName: "nav-item",
+    }
+];
