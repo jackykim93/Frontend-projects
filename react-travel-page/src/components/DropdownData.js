7f421eee7fe5863476b82d383ab6262c964707d0
jackyim93 <jackykim93@gmail.com>
2024-10-23 16:03:44 +0900

    Dropdown submenu data for hotels and airlines added

---
+++ react-travel-page/src/components/DropdownData.js
@@ -0,0 +1,59 @@
+export const HotelsData = [
+    {
+        id: 1,
+        title: 'Trivago',
+        url: 'https://www.trivago.com/',
+        cName: "submenu-item",
+    },
+    {
+        id: 2,
+        title: 'Expedia',
+        url: 'https://www.expedia.co.kr/en/',
+        cName: "submenu-item",
+    },
+    {
+        id: 3,
+        title: 'HotelsCombined',
+        url: 'https://www.hotelscombined.co.kr/?ispredir=true',
+        cName: "submenu-item",
+    },
+    {
+        id: 4,
+        title: 'Agoda',
+        url: 'https://www.agoda.com/?cid=1844104&ds=eFaxnLCiEmJ%2BJNSz',
+        cName: "submenu-item",
+    },
+    {
+        id: 5,
+        title: 'Booking',
+        url: 'https://www.booking.com/',
+        cName: "submenu-item",
+    },
+    {
+        id: 6,
+        title: 'Yanolja',
+        url: 'https://www.yanolja.com/',
+        cName: "submenu-item",
+    },
+];
+
+export const AirlinesData = [
+    {
+        id: 1,
+        title: 'Skyscanner',
+        url: 'https://www.skyscanner.co.kr/',
+        cName: "submenu-item",
+    },
+    {
+        id: 2,
+        title: 'Kayak',
+        url: 'https://www.kayak.com/flights',
+        cName: "submenu-item",
+    },
+    {
+        id: 3,
+        title: 'Cheapflights',
+        url: 'https://www.cheapflights.com/',
+        cName: "submenu-item",
+    },
+];
